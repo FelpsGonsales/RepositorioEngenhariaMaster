30f367f203e9351442d79350056e462bce101a71
Felipe <96239946+FelpsGonsales@users.noreply.github.com>
2025-02-04 18:45:12 -0300

    Palaceholder
    PalaceHolder

---
+++ src/main/java/Entidades/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import DAOs.*;
+
+/**
+ *
+ * @author a2385139
+ */
+public class Main {
+    
+}
