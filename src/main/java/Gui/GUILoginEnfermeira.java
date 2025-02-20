83ca9abb10e3b48950c5cd4cb5a6baa77a7b13b4
Pedro Henrique Mansano Franzin <pedrohenriquemansanofranzin@gmail.com>
2025-02-20 18:56:55 -0300

    Update GUILoginEnfermeira.java

--- src/main/java/Gui/GUILoginEnfermeira.java
+++ src/main/java/Gui/GUILoginEnfermeira.java
@@ -1,7 +1,6 @@
 package Gui;
 
 import DAOs.DAOenfermeira;
-//import Gui.GUIEnfermeira;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,12 +12,36 @@ public class GUILoginEnfermeira extends JFrame {
     JButton btCancelar = new JButton("Cancelar");
 
     public GUILoginEnfermeira() {
-        setTitle("Tela de Login- Enfermeira");
-        setSize(300, 150);
+        setTitle("Tela de Login - Enfermeira");
+        setSize(320, 180); // Aumentei um pouco para acomodar os botões
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
-        add(new JLabel("CIP:"));
+        // Define o fundo da janela
+        getContentPane().setBackground(new Color(209, 213, 219));
+
+        // Configurações de cores para os componentes
+        Color textColor = new Color(59, 130, 246);
+        Color borderColor = new Color(37, 99, 235);
+
+        JLabel lblCIP = new JLabel("CIP:");
+        lblCIP.setForeground(textColor);
+
+        tfCRM.setBorder(BorderFactory.createLineBorder(borderColor, 1));
+        tfCRM.setForeground(textColor);
+
+        // Aumentando o tamanho dos botões
+        Dimension buttonSize = new Dimension(120, 30);
+        btLogin.setPreferredSize(buttonSize);
+        btCancelar.setPreferredSize(buttonSize);
+
+        btLogin.setForeground(textColor);
+        btLogin.setBorder(BorderFactory.createLineBorder(borderColor, 1));
+
+        btCancelar.setForeground(textColor);
+        btCancelar.setBorder(BorderFactory.createLineBorder(borderColor, 1));
+
+        add(lblCIP);
         add(tfCRM);
         add(btLogin);
         add(btCancelar);
@@ -38,7 +61,6 @@ public class GUILoginEnfermeira extends JFrame {
                     new GUIControleEnfermeira();
                 } else {
                     JOptionPane.showMessageDialog(null, "Usuário não encontrado. Redirecionando...");
-                    // Redirecionar para a tela de CRUD
                     new GUIEnfermeira();
                 }
             }
@@ -49,10 +71,10 @@ public class GUILoginEnfermeira extends JFrame {
         setVisible(true);
     }
 
-    // Método para verificar se a enfermeira existe
-public boolean enfermeiraExiste(String cip) {
-    return new DAOenfermeira().enfermeiraExiste(cip);
-}
+    public boolean enfermeiraExiste(String cip) {
+        return new DAOenfermeira().enfermeiraExiste(cip);
+    }
+
     public static void main(String[] args) {
         new GUILoginEnfermeira();
     }
